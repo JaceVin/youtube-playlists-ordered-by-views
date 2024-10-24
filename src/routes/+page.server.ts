506bb5730b870dddcc8e4f76c556a6578ec66db7
JaceVin <denzelhdez09@gmail.com>
2024-10-24 16:06:02 -0400

    Applied types to server functions.

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,5 +1,7 @@
-export async function load({ fetch }: any): Promise<Object> {
+import type { PageServerLoad } from './$types'
+
+export const load = (async ({ fetch }) => {
     const response = await fetch('/api/test')
     const data = await response.json()
     return data
-}
+}) satisfies PageServerLoad
